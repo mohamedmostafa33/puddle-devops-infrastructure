d41aeda0bd8571a9333451a6474e6b483ab1fe1f
mohamedmostafa33 <mohamedmostafaah1@gmail.com>
2026-01-12 20:09:34 +0200

    chore(terraform): Add Terraform Cloud configuration for puddle-devops organization and workspace

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.0"
-  
+
+  cloud {
+    organization = "puddle-devops"
+
+    workspaces {
+      name = "puddle-infra"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
