e50e341e146df75e2f5cac272a2ec87da59ee86d
mohamedmostafa33 <mohamedmostafaah1@gmail.com>
2026-01-18 09:26:25 +0000

    chore(terraform): parameterize ECR repository name

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,5 @@
 resource "aws_ecr_repository" "my_ecr_repo" {
-name = "puddle-app-repo"
+name = var.ecr-name
 image_tag_mutability = "MUTABLE"
 }
+
---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "ecr-name" {
+  description = "Name of the ECR repository"
+  type        = string
+  default     = "puddle-app-repo"
+}
