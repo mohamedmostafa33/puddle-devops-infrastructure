012ec0cb71c30236fab8f80c40ff54e4c8304f63
mohamedmostafa33 <mohamedmostafaah1@gmail.com>
2026-01-18 11:36:16 +0000

    chore(terraform): add s3 bucket for media & static files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,47 @@
 resource "aws_ecr_repository" "my_ecr_repo" {
-name = var.ecr-name
-image_tag_mutability = "MUTABLE"
+  name                 = var.ecr_name
+  image_tag_mutability = "MUTABLE"
 }
 
+resource "aws_s3_bucket" "puddle_app_bucket" {
+  bucket = var.s3_bucket_name
+  tags = {
+    Name        = "puddle-app-bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "puddle_app_bucket_ownership_controls" {
+  bucket = aws_s3_bucket.puddle_app_bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "puddle_app_bucket_public_access_block" {
+  bucket = aws_s3_bucket.puddle_app_bucket.id
+
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "puddle_app_bucket_versioning" {
+  bucket = aws_s3_bucket.puddle_app_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "puddle_app_bucket_sse" {
+  bucket = aws_s3_bucket.puddle_app_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,11 @@
-variable "ecr-name" {
+variable "ecr_name" {
   description = "Name of the ECR repository"
   type        = string
   default     = "puddle-app-repo"
+}
+
+variable "s3_bucket_name" {
+  description = "Name of the s3 bucket"
+  type        = string
+  default     = "puddle-s3-media-nti"
 }
